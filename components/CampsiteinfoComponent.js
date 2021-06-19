f5b884399068b41bc196fde0bddf1277db14bd2e
Kianush Dixon <kianushdixon@gmail.com>
2021-06-18 21:54:39 -0500

    stupid Android Emulator

--- components/CampsiteinfoComponent.js
+++ components/CampsiteinfoComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { CAMPSITES }  from '../shared/campsites';
 
 function RenderCampsite({campsite}){
     if (campsite){
@@ -18,9 +19,26 @@ function RenderCampsite({campsite}){
     return <View />
 }
 
-function CampsiteInfo(props) {
+class CampsiteInfo extends Component {
 
-return <RenderCampsite campsite={props.campsite} />;
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Campsite Information'
+    }
+
+    render() {
+
+        const campsiteId = this.props.navigation.getParam('campsiteId');
+        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+
+        return <RenderCampsite campsite={campsite} />;
+        }
+    }
 
 export default CampsiteInfo;
